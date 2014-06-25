5934de62fbc30f35e577641187336aa7c3ae49ab
Phan Tien Dong <eastward2107@gmail.com>
2014-06-25 10:48:26 +0700

    remove hard-coded data, add modal to create new task

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,35 @@
 // Ionic Todo Controller
 
 angular.module('todo', ['ionic'])
-.controller('TodoController', function($scope) {
-  $scope.tasks = [
-    {title: 'Check all notifications'},
-    {title: 'Meet Jack at 8 a.m'},
-    {title: 'Call back to customer at 10 a.m'},
-    {title: 'Back to office for meeting request'},
-    {title: 'Go to cinema with Tom'}
-  ];
+
+.controller('TodoController', function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
